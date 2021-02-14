1357a02f1e153f6bb12ce199159312cffe9229ec
A1caida <nimmt74@gmail.com>
2021-02-14 10:45:13 +0400

    oops

--- lab2_zar/MainWindow.xaml.cs
+++ lab2_zar/MainWindow.xaml.cs
@@ -12,107 +12,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using MySql.Data.MySqlClient;
-using System.Data.Common;
+
 
 
 namespace lab2_zar
 {
-     
     public partial class MainWindow : Window
     {
-        //struct str
-        //{
-        //    public string id;
-        //    public string familia;
-        //    public string imya;
-        //    public string otchestvo;
-        //    public string rabota;
-        //    public string zarplata;
-        //}
-
-        //MySqlConnection uwu;
-
-        //List<str> kek()
-        //{
-        //    MySqlConnectionStringBuilder stringBuilder = new MySqlConnectionStringBuilder();
-        //    stringBuilder.Server = "localhost";
-        //    stringBuilder.UserID = "root";
-        //    stringBuilder.Database = "a1caida";
-        //    stringBuilder.SslMode = MySqlSslMode.None;
-        //    uwu = new MySqlConnection(stringBuilder.ConnectionString);
-
-        //    MySqlCommand command = uwu.CreateCommand();
-        //    command.CommandText = "SELECT uwu.id,`surname`, `name`, `otch`,`work`,`salary` FROM a1caida.uwu JOIN  a1caida.owo ON uwu.id_fio = owo.id";
-
-        //    List<str> bd = new List<str>();
-
-        //    try
-        //    {
-        //        uwu.Open();
-        //        using (DbDataReader reader = command.ExecuteReader())
-        //        {
-        //            while (reader.Read())
-        //            {
-        //                str databd = new str
-        //                {
-        //                    id = reader.GetString(0),
-        //                    familia = reader.GetString(1),
-        //                    imya = reader.GetString(2),
-        //                    otchestvo = reader.GetString(3),
-        //                    rabota = reader.GetString(4),
-        //                    zarplata = reader.GetString(5),
-        //                };
-        //                bd.Add(databd);
-        //            }
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        MessageBox.Show(ex.Message);
-        //    }
-        //    return bd;
-
-        //}
-        //List<str> lol()
-        //{
-        //    MySqlConnectionStringBuilder stringBuilder = new MySqlConnectionStringBuilder();
-        //    stringBuilder.Server = "localhost";
-        //    stringBuilder.UserID = "root";
-        //    stringBuilder.Database = "a1caida";
-        //    stringBuilder.SslMode = MySqlSslMode.None;
-        //    uwu = new MySqlConnection(stringBuilder.ConnectionString);
-
-        //    MySqlCommand command = uwu.CreateCommand();
-        //    command.CommandText = "SELECT `id`,`surname`, `name`, `otch` FROM a1caida.owo";
-
-        //    List<str> bd = new List<str>();
-
-        //    try
-        //    {
-        //        uwu.Open();
-        //        using (DbDataReader reader = command.ExecuteReader())
-        //        {
-        //            while (reader.Read())
-        //            {
-        //                str databd = new str
-        //                {
-        //                    id = reader.GetString(0),
-        //                    familia = reader.GetString(1),
-        //                    imya = reader.GetString(2),
-        //                    otchestvo = reader.GetString(3),        
-        //                };
-        //                bd.Add(databd);
-        //            }
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        MessageBox.Show(ex.Message);
-        //    }
-        //    return bd;
-
-        //}
         public MainWindow()
         {
             InitializeComponent();
