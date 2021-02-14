04e37592b9711133ed897d38089bc4ebdd41aa4a
A1caida <nimmt74@gmail.com>
2021-02-14 10:44:13 +0400

    make the class in another file

---
+++ lab2_zar/Class1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+
+namespace lab2_zar
+{
+    class db
+    {
+        MySqlConnection Connection;
+
+        public db(string server, string user, string pass, string database)
+        {
+            MySqlConnectionStringBuilder Connect = new MySqlConnectionStringBuilder
+            {
+                Server = server,
+                UserID = user,
+                Password = pass,
+                Port = 3306,
+                Database = database,
+                CharacterSet = "utf8"
+            };
+            Connection = new MySqlConnection(Connect.ConnectionString);
+        }
+        public long insert(string surname, string namee, string otch)
+        {
+            MySqlCommand command = Connection.CreateCommand();
+            command.CommandText = "INSERT INTO owo(surname, name, otch) VALUES(?surname, ?name, ?otch)";
+            command.Parameters.Add("?surname", MySqlDbType.VarChar).Value = surname;
+            command.Parameters.Add("?name", MySqlDbType.VarChar).Value = namee;
+            command.Parameters.Add("?otch", MySqlDbType.VarChar).Value = otch;
+
+            try
+            {
+                Connection.Open();
+                command.ExecuteNonQuery();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Connection.Close();
+            }
+            return -1;
+        }
+
+        public long insert(int id_fio, string work, int salary)
+        {
+            MySqlCommand command = Connection.CreateCommand();
+            command.CommandText = "INSERT INTO uwu(id_fio, work, salary) VALUES(?id_fio, ?work, ?salary)";
+            command.Parameters.Add("?id_fio", MySqlDbType.Int32).Value = id_fio;
+            command.Parameters.Add("?work", MySqlDbType.VarChar).Value = work;
+            command.Parameters.Add("?salary", MySqlDbType.Int32).Value = salary;
+
+            try
+            {
+                Connection.Open();
+                command.ExecuteNonQuery();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Connection.Close();
+            }
+            return -1;
+        }
+
+        public long delete(int id)
+        {
+            MySqlCommand command = Connection.CreateCommand();
+            command.CommandText = "DELETE FROM a1caida.uwu WHERE `id` =(?id)";
+            command.Parameters.Add("?id", MySqlDbType.Int32).Value = id;
+
+            try
+            {
+                Connection.Open();
+                command.ExecuteNonQuery();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Connection.Close();
+            }
+            return -1;
+        }
+
+        public struct str
+        {
+            public string id;
+            public string familia;
+            public string imya;
+            public string otchestvo;
+            public string rabota;
+            public string zarplata;
+        }
+
+        public List<str> kek()
+        {
+            MySqlCommand command = Connection.CreateCommand();
+            command.CommandText = "SELECT uwu.id,`surname`, `name`, `otch`,`work`,`salary` FROM a1caida.uwu JOIN  a1caida.owo ON uwu.id_fio = owo.id";
+
+            List<str> bd = new List<str>();
+
+            try
+            {
+                Connection.Open();
+                using (DbDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        str databd = new str
+                        {
+                            id = reader.GetString(0),
+                            familia = reader.GetString(1),
+                            imya = reader.GetString(2),
+                            otchestvo = reader.GetString(3),
+                            rabota = reader.GetString(4),
+                            zarplata = reader.GetString(5),
+                        };
+                        bd.Add(databd);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return bd;
+
+        }
+        public List<str> lol()
+        {
+            MySqlCommand command = Connection.CreateCommand();
+            command.CommandText = "SELECT `id`,`surname`, `name`, `otch` FROM a1caida.owo";
+
+            List<str> bd = new List<str>();
+
+            try
+            {
+                Connection.Open();
+                using (DbDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        str databd = new str
+                        {
+                            id = reader.GetString(0),
+                            familia = reader.GetString(1),
+                            imya = reader.GetString(2),
+                            otchestvo = reader.GetString(3),
+                        };
+                        bd.Add(databd);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return bd;
+
+        }
+    }
+}
--- lab2_zar/MainWindow.xaml.cs
+++ lab2_zar/MainWindow.xaml.cs
@@ -18,195 +18,101 @@ using System.Data.Common;
 
 namespace lab2_zar
 {
-
-
-    class db
-    {
-        MySqlConnection Connection;
-
-        public db(string server, string user, string pass, string database)
-        {
-            MySqlConnectionStringBuilder Connect = new MySqlConnectionStringBuilder
-            {
-                Server = server,
-                UserID = user,
-                Password = pass,
-                Port = 3306,
-                Database = database,
-                CharacterSet = "utf8"
-            };
-            Connection = new MySqlConnection(Connect.ConnectionString);
-        }
-        public long insert(string surname, string namee, string otch)
-        {
-            MySqlCommand command = Connection.CreateCommand();
-            command.CommandText = "INSERT INTO owo(surname, name, otch) VALUES(?surname, ?name, ?otch)";
-            command.Parameters.Add("?surname", MySqlDbType.VarChar).Value = surname;
-            command.Parameters.Add("?name", MySqlDbType.VarChar).Value = namee;
-            command.Parameters.Add("?otch", MySqlDbType.VarChar).Value = otch;
-
-            try
-            {
-                Connection.Open();
-                command.ExecuteNonQuery();
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                Connection.Close();
-            }
-            return -1;
-        }
-
-        public long insert(int id_fio, string work, int salary)
-        {
-            MySqlCommand command = Connection.CreateCommand();
-            command.CommandText = "INSERT INTO uwu(id_fio, work, salary) VALUES(?id_fio, ?work, ?salary)";
-            command.Parameters.Add("?id_fio", MySqlDbType.Int32).Value = id_fio;
-            command.Parameters.Add("?work", MySqlDbType.VarChar).Value = work;
-            command.Parameters.Add("?salary", MySqlDbType.Int32).Value = salary;
-
-            try
-            {
-                Connection.Open();
-                command.ExecuteNonQuery();
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                Connection.Close();
-            }
-            return -1;
-        }
-
-        public long delete(int id)
-        {
-            MySqlCommand command = Connection.CreateCommand();
-            command.CommandText = "DELETE FROM a1caida.uwu WHERE `id` =(?id)";
-            command.Parameters.Add("?id", MySqlDbType.Int32).Value = id;
-           
-
-            try
-            {
-                Connection.Open();
-                command.ExecuteNonQuery();
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                Connection.Close();
-            }
-            return -1;
-        }
-    }
-
-
+     
     public partial class MainWindow : Window
     {
-        struct str
-        {
-            public string id;
-            public string familia;
-            public string imya;
-            public string otchestvo;
-            public string rabota;
-            public string zarplata;
-        }
+        //struct str
+        //{
+        //    public string id;
+        //    public string familia;
+        //    public string imya;
+        //    public string otchestvo;
+        //    public string rabota;
+        //    public string zarplata;
+        //}
 
-        MySqlConnection uwu;
+        //MySqlConnection uwu;
 
-        List<str> kek()
-        {
-            MySqlConnectionStringBuilder stringBuilder = new MySqlConnectionStringBuilder();
-            stringBuilder.Server = "localhost";
-            stringBuilder.UserID = "root";
-            stringBuilder.Database = "a1caida";
-            stringBuilder.SslMode = MySqlSslMode.None;
-            uwu = new MySqlConnection(stringBuilder.ConnectionString);
+        //List<str> kek()
+        //{
+        //    MySqlConnectionStringBuilder stringBuilder = new MySqlConnectionStringBuilder();
+        //    stringBuilder.Server = "localhost";
+        //    stringBuilder.UserID = "root";
+        //    stringBuilder.Database = "a1caida";
+        //    stringBuilder.SslMode = MySqlSslMode.None;
+        //    uwu = new MySqlConnection(stringBuilder.ConnectionString);
 
-            MySqlCommand command = uwu.CreateCommand();
-            command.CommandText = "SELECT uwu.id,`surname`, `name`, `otch`,`work`,`salary` FROM a1caida.uwu JOIN  a1caida.owo ON uwu.id_fio = owo.id";
+        //    MySqlCommand command = uwu.CreateCommand();
+        //    command.CommandText = "SELECT uwu.id,`surname`, `name`, `otch`,`work`,`salary` FROM a1caida.uwu JOIN  a1caida.owo ON uwu.id_fio = owo.id";
 
-            List<str> bd = new List<str>();
+        //    List<str> bd = new List<str>();
 
-            try
-            {
-                uwu.Open();
-                using (DbDataReader reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        str databd = new str
-                        {
-                            id = reader.GetString(0),
-                            familia = reader.GetString(1),
-                            imya = reader.GetString(2),
-                            otchestvo = reader.GetString(3),
-                            rabota = reader.GetString(4),
-                            zarplata = reader.GetString(5),
-                        };
-                        bd.Add(databd);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            return bd;
+        //    try
+        //    {
+        //        uwu.Open();
+        //        using (DbDataReader reader = command.ExecuteReader())
+        //        {
+        //            while (reader.Read())
+        //            {
+        //                str databd = new str
+        //                {
+        //                    id = reader.GetString(0),
+        //                    familia = reader.GetString(1),
+        //                    imya = reader.GetString(2),
+        //                    otchestvo = reader.GetString(3),
+        //                    rabota = reader.GetString(4),
+        //                    zarplata = reader.GetString(5),
+        //                };
+        //                bd.Add(databd);
+        //            }
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //    }
+        //    return bd;
 
-        }
-        List<str> lol()
-        {
-            MySqlConnectionStringBuilder stringBuilder = new MySqlConnectionStringBuilder();
-            stringBuilder.Server = "localhost";
-            stringBuilder.UserID = "root";
-            stringBuilder.Database = "a1caida";
-            stringBuilder.SslMode = MySqlSslMode.None;
-            uwu = new MySqlConnection(stringBuilder.ConnectionString);
+        //}
+        //List<str> lol()
+        //{
+        //    MySqlConnectionStringBuilder stringBuilder = new MySqlConnectionStringBuilder();
+        //    stringBuilder.Server = "localhost";
+        //    stringBuilder.UserID = "root";
+        //    stringBuilder.Database = "a1caida";
+        //    stringBuilder.SslMode = MySqlSslMode.None;
+        //    uwu = new MySqlConnection(stringBuilder.ConnectionString);
 
-            MySqlCommand command = uwu.CreateCommand();
-            command.CommandText = "SELECT `id`,`surname`, `name`, `otch` FROM a1caida.owo J";
+        //    MySqlCommand command = uwu.CreateCommand();
+        //    command.CommandText = "SELECT `id`,`surname`, `name`, `otch` FROM a1caida.owo";
 
-            List<str> bd = new List<str>();
+        //    List<str> bd = new List<str>();
 
-            try
-            {
-                uwu.Open();
-                using (DbDataReader reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        str databd = new str
-                        {
-                            id = reader.GetString(0),
-                            familia = reader.GetString(1),
-                            imya = reader.GetString(2),
-                            otchestvo = reader.GetString(3),        
-                        };
-                        bd.Add(databd);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            return bd;
+        //    try
+        //    {
+        //        uwu.Open();
+        //        using (DbDataReader reader = command.ExecuteReader())
+        //        {
+        //            while (reader.Read())
+        //            {
+        //                str databd = new str
+        //                {
+        //                    id = reader.GetString(0),
+        //                    familia = reader.GetString(1),
+        //                    imya = reader.GetString(2),
+        //                    otchestvo = reader.GetString(3),        
+        //                };
+        //                bd.Add(databd);
+        //            }
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //    }
+        //    return bd;
 
-        }
+        //}
         public MainWindow()
         {
             InitializeComponent();
@@ -227,12 +133,11 @@ namespace lab2_zar
             {
                 MessageBox.Show("ok");
             }
-
         }
 
         private void vivodrab(object sender, RoutedEventArgs e)
         {
-            MainWindow Kurisu = new MainWindow();
+            db Kurisu = new db("127.0.0.1", "root", "", "a1caida");
             var Maki = Kurisu.kek();
             view.Text = "";
             foreach (var data in Maki)
@@ -243,7 +148,7 @@ namespace lab2_zar
 
         private void vivodfio(object sender, RoutedEventArgs e)
         {
-            MainWindow Kurisu = new MainWindow();
+            db Kurisu = new db("127.0.0.1", "root", "", "a1caida");
             var Maki = Kurisu.lol();
             view1.Text = "";
             foreach (var data in Maki)
